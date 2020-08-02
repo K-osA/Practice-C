4cc75ddd094c6c5ddc1241def41b0a633b292f6f
Sangwon Ko <evbayooooo@gmail.com>
2020-08-02 13:39:14 +0000

    Chapter 13-3

--- 12-3/3.c
+++ 12-3/3.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main()
 {
-	int arr[3]={1,2,3};
-	int (*parr)[3]=&arr;
-
-
-	printf("arr[1]:%d\n",arr[1]);
+	int arr[2][3]={{1,2,3},{4,5,6}};
+	int (*parr)[3]=arr;
+	
+	printf("parr[0][1]:%d\n",*(*parr+4));
+	
+	/*printf("arr[1]:%d\n",arr[1]);
 	printf("parr[1]:%d\n",(**parr+1));
 
 	printf("arr:%p\n",arr);
-	printf("parr:%p\n",parr);
+	printf("parr:%p\n",parr);*/
 	return 0;
 }
---
+++ 13-3/3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int read_val(const int val);
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	read_val(a);
+	return 0;
+}
+int read_val(const int val){
+//	val=5;
+	return 0;
+}
