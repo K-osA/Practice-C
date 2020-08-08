58bb4f8a76e9bcc383a520d40bc391a3a6d1aa99
Sangwon Ko <evbayooooo@gmail.com>
2020-08-08 01:34:05 +0000

    Chapter 20_2

--- 20-2/node-adv.c
+++ 20-2/node-adv.c
@@ -3,13 +3,23 @@
 struct Node{
 	int data;
 	struct Node* nextNode;
+	struct Node* prevNode;
 };
 struct Node* CreateNode(int);
 struct Node* InsertNode(struct Node*, int);
-struct Node* DestroyNode(struct Node*, struct Node*);
+void DestroyNode(struct Node*, struct Node*);
+void PrintNodeFrom(struct Node*);
+int CountNode(struct Node*);
+int SearchNode(struct Node*, struct Node*);
 int main()
 {
 	struct Node* node1=CreateNode(100);
+	struct Node* node2=InsertNode(node1, 200);
+	struct Node* node3=InsertNode(node2, 300);
+	struct Node* node4=InsertNode(node2, 400);
+
+	PrintNodeFrom(node1);
+	printf("%d\n",SearchNode(node1, node3));
 	return 0;
 }
 
@@ -17,6 +27,7 @@ struct Node* CreateNode(int data){
 	struct Node* newNode=(struct Node*)malloc(sizeof(struct Node));
 
 	newNode->data=data;
+	newNode->prevNode=NULL;
 	newNode->nextNode=NULL;
 
 	return newNode;
@@ -24,6 +35,10 @@ struct Node* CreateNode(int data){
 
 struct Node* InsertNode(struct Node* currentNode, int data){
 	struct Node* afterNode = CreateNode(data);
+
+	afterNode->prevNode=currentNode;
+	if(currentNode->nextNode)
+	currentNode->nextNode->prevNode=afterNode;
 
 	afterNode->nextNode=currentNode->nextNode;
 	currentNode->nextNode=afterNode;
@@ -45,4 +60,25 @@ void DestroyNode(struct Node* destroy, struct Node* head){
 	}
 	return;
 }
-void PrintNode(struct 
+void PrintNodeFrom(struct Node* from){
+	while(from){
+		printf("prev : %p\ncurrent : %p\nnext : %p\ndata : %d\n",from->prevNode, from, from->nextNode, from->data);
+		printf("-----------------------------------------\n");
+		from=from->nextNode;
+	}
+}
+int CountNode(struct Node* head){
+	int cnt=0;
+	while(head){
+		cnt++;
+		head=head->nextNode;
+	}
+	return cnt;
+}
+int SearchNode(struct Node* head, struct Node *search){
+	while(head){
+		if(head==search)
+			return head->data;
+		head=head->nextNode;
+	}
+}
